2376d8339045b215083b2741d66531dbdedafb9a
Palak Richhariya <palakrichhariya1012@gmail.com>
2026-07-23 16:11:37 +0530

    feat: add Bubble Sort implementation

---
+++ src/Recursion/BubbleSort.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] num = new int[] {5,4,3,2,1};
+        bubbleSort(num);
+        System.out.println(Arrays.toString(num));
+    }
+
+    static void bubbleSort(int[] arr){
+
+    }
+}
