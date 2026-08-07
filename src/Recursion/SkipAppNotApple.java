b595e7caab9ae1b7adc7e2193648514466af1855
Palak Richhariya <palakrichhariya1012@gmail.com>
2026-08-07 12:26:37 +0530

    Add recursive solution to skip app but not apple

---
+++ src/Recursion/SkipAppNotApple.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class SkipAppNotApple {
+    static String skip(String up){
+        if(up.isEmpty())return "";
+
+        if(up.startsWith("app") && !up.startsWith("apple")){
+            return skip(up.substring(3));
+        }else{
+            return up.charAt(0) + skip(up.substring(1));
+        }
+    }
+}
